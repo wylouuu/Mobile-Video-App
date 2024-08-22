db33750ed0b39c19c1ede066a278a7ecfe02d350
wylouuu <lojokowilly@gmail.com>
2024-08-22 12:28:18 +0700

    implement search screen

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Alert,
+  FlatList,
+  Image,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import { getAllPosts, getLatestPost, searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <>
+      <SafeAreaView className="bg-primary  h-full">
+        <FlatList
+          data={posts}
+          // data={[]}
+          keyExtractor={(item) => item.$id}
+          renderItem={({ item }) => <VideoCard video={item} />}
+          ListHeaderComponent={() => (
+            <View className="my-6 px-4">
+              <Text className="font-pmedium text-sm text-gray-100">
+                Search Results
+              </Text>
+              <Text className="text-2xl font-psemibold text-white">
+                {query}
+              </Text>
+              <View className="mt-6 mb-8">
+                <SearchInput initialQuery={query} refetch={refetch} />
+              </View>
+            </View>
+          )}
+          ListEmptyComponent={() => (
+            <EmptyState
+              title="No Videos Found"
+              subtitle="No videos found for this search query"
+            />
+          )}
+        />
+      </SafeAreaView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
